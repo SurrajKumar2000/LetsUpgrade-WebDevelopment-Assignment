d6c5700e379ff41f6d85e3333aa525c7c6478a49
SK <itzmesk2000@gmail.com>
2021-03-26 18:22:30 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,1 +1,17 @@
-module.exports = require('./head');
+//To fetch by Tag Name
+
+var ele = document.getElementByTagName('h1');
+console.log(ele[0]);
+ele[0].innerText='Good Afternoon';
+
+//To fetch by Class Name
+
+var elem = document.getElementByClassName('op');
+console.log(elem[0]);
+elem[0].innerText='Good';
+
+//To fetch by Id
+
+var eleme = document.getElementById('dd');
+console.log(eleme[0]);
+eleme[0].innerText='Hi , How are you?';
