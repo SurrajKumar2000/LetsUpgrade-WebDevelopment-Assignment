1a34703474b3b191f8316dff3b9baa2fe6770109
SK <itzmesk2000@gmail.com>
2021-03-26 18:55:09 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,17 +1,15 @@
 //To fetch by Tag Name
 
-var ele = document.getElementByTagName('h1');
+var ele = document.getElementsByTagName('h1');
 console.log(ele[0]);
-ele[0].innerText='Good Afternoon';
+ele[0].innerText="Good Noon";
 
 //To fetch by Class Name
 
-var elem = document.getElementByClassName('op');
+var elem = document.getElementsByClassName('op');
 console.log(elem[0]);
-elem[0].innerText='Good';
 
 //To fetch by Id
 
 var eleme = document.getElementById('dd');
 console.log(eleme[0]);
-eleme[0].innerText='Hi , How are you?';
